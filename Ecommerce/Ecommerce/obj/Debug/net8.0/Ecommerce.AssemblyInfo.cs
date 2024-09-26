1bf06c8a71aeb324b20a03adbdbe6976e3371875
Sandaru Jayathilaka <sandarujayathilaka26@gmail.com>
2024-09-26 10:47:54 +0530

    mongo connectionadded

--- Ecommerce/Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52e7533b2a73dfab3e2418c79e39a8f6916aefca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ca93735ed510c6b811a4e2d2776a4e92987d324")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
