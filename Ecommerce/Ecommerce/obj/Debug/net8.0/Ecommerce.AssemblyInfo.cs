342cff8080b0c1c9ce81b02e54b41d4a0fca1e3a
Sandaru Jayathilaka <sandarujayathilaka26@gmail.com>
2024-09-26 10:23:52 +0530

    ProductDto added

--- Ecommerce/Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d08c6856b3811ad5693584edc0ac74cff3a0d873")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3960e032bda65fc93632aaeda049f4e8c700dfc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
