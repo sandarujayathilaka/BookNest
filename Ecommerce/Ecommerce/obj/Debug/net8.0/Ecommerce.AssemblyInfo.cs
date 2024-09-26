c4f114d9f75966ae59b91508386fe978d4228e45
Sandaru Jayathilaka <sandarujayathilaka26@gmail.com>
2024-09-26 10:49:49 +0530

    Notificationctrladded

--- Ecommerce/Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf06c8a71aeb324b20a03adbdbe6976e3371875")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e828e545ad857906829ded0a9a715c42b6ae6d2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
