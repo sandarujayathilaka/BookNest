de491f9221a0f69f2c6fb060040c8986ea8cae83
Sandaru Jayathilaka <sandarujayathilaka26@gmail.com>
2024-09-30 00:00:40 +0530

    Update orderDto

--- web-service/Ecommerce/Dto/OrderDto.cs
+++ web-service/Ecommerce/Dto/OrderDto.cs
@@ -8,6 +8,7 @@
         public string Status { get; set; }
         public Boolean orderCancelation { get; set; }
         public string cancelationNote { get; set; }
+        public string CancelationOfficerNote { get; set; }
     }
 
     public class ProductOrderDto
