0462403fe9519ab83638f1d33048a5516f000f36
Sandaru Jayathilaka <sandarujayathilaka26@gmail.com>
2024-10-03 11:13:09 +0530

    delete file

--- Mobile app/eadapp/app/src/main/java/com/ead/eadapp/Product.kt
+++
@@ -1,9 +0,0 @@
-package com.ead.eadapp
-
-data class Product(
-    val id: String,
-    val name: String,
-    val description: String,
-    val price: Double,
-    val imageUrl: String
-)
--- Mobile app/eadapp/app/src/main/java/com/ead/eadapp/RegisterRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.ead.eadapp
-
-data class RegisterRequest(
-    val fullName: String,
-    val email: String,
-    val passwordHash: String,
-    val role: String = "Customer" // Default role is 'Customer'
-)
